f62103b283e13d7fea90817270c77b8e5c5e9fc6
Petrus743 <petrusagung12@gmail.com>
2024-06-15 15:28:26 +0700

    npm install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// *
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // AUTH
-import Login from './Auth/Login';
-import Register from './Auth/Register';
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
 
 // PAGES
-import Home from './Pages/User/Home';
-import AboutUs from './Pages/User/AboutUs';
-import Contact from './Pages/User/Contact';
-import Category from './Pages/User/Category';
-import DetailProduct from './Pages/User/DetailProduct';
-import Product from './Pages/User/Product';
-import NotFoundPage from './Pages/User/NotFoundPage';
-import Vitamin from './Pages/User/VitaminC';
-import Checkout from './Pages/User/Checkout';
+import Home from "./Pages/User/Home";
+import AboutUs from "./Pages/User/AboutUs";
+import Contact from "./Pages/User/Contact";
+import Category from "./Pages/User/Category";
+import DetailProduct from "./Pages/User/DetailProduct";
+import Product from "./Pages/User/Product";
+import NotFoundPage from "./Pages/User/NotFoundPage";
+import Vitamin from "./Pages/User/VitaminC";
+import Checkout from "./Pages/User/Checkout";
 
 // DASHBOARD
-import DashboardPage from './Pages/Admin/DashboardPage';
-import MyProfilePage from './Pages/Admin/MyProfilePage';
-import ProductList from './Pages/Admin/ProductList';
-import UserListPage from './Pages/Admin/UserListPage';
-import AddAdmin from './Pages/Admin/AddAdmin';
-
+import DashboardPage from "./Pages/Admin/DashboardPage";
+import MyProfilePage from "./Pages/Admin/MyProfilePage";
+import ProductList from "./Pages/Admin/ProductList";
+import UserListPage from "./Pages/Admin/UserListPage";
+import AddAdmin from "./Pages/Admin/AddAdmin";
 
 const App = () => {
-
   return (
     <Router>
       <div className="app-container">
-
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
@@ -46,40 +44,15 @@ const App = () => {
           <Route path="/checkout" element={<Checkout />} />
 
           {/* DASHBOARD */}
-          <Route
-            path="/dashboard"
-            element={
-              <DashboardPage />
-            }
-          />
-          <Route
-            path="/dashboard/my-profile"
-            element={
-              <MyProfilePage />
-            }
-          />
-          <Route
-            path="/dashboard/product-list"
-            element={
-              <ProductList />
-            }
-          />
-          <Route
-            path="/dashboard/user-list"
-            element={
-              <UserListPage />
-            }
-          />
-          <Route
-            path="/dashboard/add-admin"
-            element={
-              <AddAdmin />
-            }
-          />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/dashboard/my-profile" element={<MyProfilePage />} />
+          <Route path="/dashboard/product-list" element={<ProductList />} />
+          <Route path="/dashboard/user-list" element={<UserListPage />} />
+          <Route path="/dashboard/add-admin" element={<AddAdmin />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
